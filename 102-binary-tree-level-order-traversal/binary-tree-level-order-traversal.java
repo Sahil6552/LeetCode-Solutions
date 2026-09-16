18e3aa33f3e11b92cd7427e9e4640b74f91a7eb7
Sahil6552 <sahil1528kumar@gmail.com>
2026-09-17 00:55:41 +0530

    Time: 1 ms (95.89%) | Memory: 46.7 MB (61.51%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -13,36 +13,36 @@
  *     }
  * }
  */
- class pair{
-    TreeNode node;
-    int level;
-    pair(TreeNode node, int level){
-        this.node = node;
-        this.level = level;
-    }
- }
+//  class pair{
+//     TreeNode node;
+//     int level;
+//     pair(TreeNode node, int level){
+//         this.node = node;
+//         this.level = level;
+//     }
+//  }
  
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> ans = new  ArrayList<>();
         if(root==null) return ans;
-        Queue<pair> q = new LinkedList<>();
-        q.add(new pair(root,0));
-        int currlevel = 0;
-        ArrayList<Integer> list = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
         while(q.size()>0){
-            pair front = q.remove();
-            if(front.level!=currlevel){
-                ans.add(list);
-                list = new ArrayList<>();
-                currlevel++;
+            ArrayList<Integer> list = new ArrayList<>();
+            int level = q.size();
+            for(int i = 0;i<level;i++){
+                TreeNode front = q.poll();
+                list.add(front.val);
+                if(front.left!=null){
+                    q.add(front.left);
+                }
+                if(front.right!=null){
+                    q.add(front.right);
+                }
             }
-            list.add(front.node.val);
-            if(front.node.left!=null) q.add(new pair(front.node.left,front.level+1));
-            if(front.node.right!=null) q.add(new pair(front.node.right,front.level+1));
-            
+            ans.add(list);
         }
-        ans.add(list);
         return ans;
     }
 }
